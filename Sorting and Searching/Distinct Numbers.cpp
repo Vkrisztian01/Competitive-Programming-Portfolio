5e8a38fee912d9d1ad209cd7eefd11af094fb630
Vaszary Krisztián <krisztian.vaszary@gmail.com>
2024-10-06 15:12:19 +0200

    Update Distinct Numbers.cpp

--- Sorting and Searching/Distinct Numbers.cpp
+++ Sorting and Searching/Distinct Numbers.cpp
@@ -1,1 +1,23 @@
+// Problem Name: Distinct Numbers
+// Link to The Problem: https://cses.fi/problemset/task/1621
 
+#include <iostream>
+#include<set>
+ 
+using namespace std;
+ 
+int main()
+{
+    set<long long int>nums;
+    long long int n;
+    cin>>n;
+    while(n)
+    {
+        --n;
+        long long int a;
+        cin>>a;
+        nums.insert(a);
+    }
+    cout<<nums.size();
+    return 0;
+}
