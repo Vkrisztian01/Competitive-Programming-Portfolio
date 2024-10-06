17f896f8a7b4b0fab5e7004db409e783a41a8c8f
Vaszary Krisztián <krisztian.vaszary@gmail.com>
2024-10-06 14:53:06 +0200

    Add files via upload

---
+++ DistinctNumbers-Sorting_and_Searching.cpp
@@ -0,0 +1,25 @@
+//Name: Distinct Numbers
+//Category: Sorting and Searchning
+//Link: https://cses.fi/problemset/task/1621
+
+
+#include <iostream>
+#include<set>
+ 
+using namespace std;
+ 
+int main()
+{
+    set<long long int>nums;
+    long long int n;
+    cin>>n;
+    while(n)
+    {
+        --n;
+        long long int a;
+        cin>>a;
+        nums.insert(a);
+    }
+    cout<<nums.size();
+    return 0;
+}
