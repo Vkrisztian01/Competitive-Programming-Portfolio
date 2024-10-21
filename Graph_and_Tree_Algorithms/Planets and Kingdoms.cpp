60706bb460f3c99b21fb7802cc4df91140ec0fd0
Vaszary Krisztián <krisztian.vaszary@gmail.com>
2024-10-21 19:44:38 +0200

    Create Planets and Kingdoms.cpp

---
+++ Graph_and_Tree_Algorithms/Planets and Kingdoms.cpp
@@ -0,0 +1,53 @@
+// Problem Name: Planets and Kingdoms
+// Link to the Problem: https://cses.fi/problemset/task/1683
+// Link to the Solution: https://cses.fi/paste/df3062fc4656c87e950565/
+
+#include <iostream>
+#include<bits/stdc++.h>
+typedef long long ll; using ll = long long;
+ 
+using namespace std;
+ 
+void dfs1(vector<vector<int> > &adj,vector<bool> &visited,stack<int> &order, int v)
+{
+    visited[v]=true;
+    for(int to:adj[v]) if(!visited[to]) dfs1(adj,visited,order,to);
+    order.push(v);
+}
+ 
+void dfs2(vector<vector<int> > &revadj,vector<bool> &visited,vector<int> &scc,int cnt,int v)
+{
+    visited[v]=true;
+    scc[v]=cnt;
+    for(int from:revadj[v])
+    {
+        if(!visited[from]) dfs2(revadj,visited,scc,cnt,from);
+    }
+}
+ 
+int main()
+{
+    int n,m; cin>>n>>m; int cnt=0;
+    vector<vector<int> > adj(n+1);vector<vector<int> > revadj(n+1); vector<bool> visited(n+1,false);stack<int> order; vector<int> scc(n+1); vector<int> ans;
+    for(int i=1;i<=m;i++)
+    {
+        int a,b; cin>>a>>b;
+        adj[a].push_back(b); revadj[b].push_back(a);
+    }
+    for(int v=1;v<=n;v++) {if(!visited[v]) dfs1(adj,visited,order,v);}
+    visited.assign(n+1,false);
+    while(!order.empty())
+    {
+        int v=order.top(); order.pop();
+        if(visited[v]) continue;
+        else
+        {
+            ans.push_back(v);
+            dfs2(revadj,visited,scc,cnt,v);
+            cnt++;
+        }
+    }
+    cout<<cnt<<endl;
+    for(int v=1;v<=n;v++) cout<<scc[v]+1<<" ";
+    return 0;
+}
